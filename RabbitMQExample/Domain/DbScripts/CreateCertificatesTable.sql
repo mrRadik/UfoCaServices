b1fefa92cf56e28a12490582c77cfaf5e2fe2555
Radik F <noname_rf@mail.ru>
2022-09-17 21:40:35 +0500

    Added sql scripts

---
+++ RabbitMQExample/Domain/DbScripts/CreateCertificatesTable.sql
@@ -0,0 +1,16 @@
+﻿create table "Certificates"
+(
+    "Id"         integer default nextval('certificates_id_seq'::regclass) not null
+        constraint certificates_pkey
+            primary key,
+    "CertId"     integer                                                  not null
+        constraint certificates_certid_key
+            unique,
+    "Issuer"     varchar(255)                                             not null,
+    "NotAfter"   bigint                                                   not null,
+    "NotBefore"  bigint                                                   not null,
+    "Serial"     varchar(50)                                              not null,
+    "Subject"    varchar                                                  not null,
+    "Thumbprint" varchar(50)                                              not null,
+    "Data"       varchar                                                  not null
+);
---
+++ RabbitMQExample/Domain/DbScripts/CreateLogsTable.sql
@@ -0,0 +1,10 @@
+﻿create table "Logs"
+(
+    "Id"          integer default nextval('logs_id_seq'::regclass) not null
+        constraint logs_pkey
+            primary key,
+    "Type"        varchar(50)                                      not null,
+    "Application" varchar(50)                                      not null,
+    "Message"     varchar(255)                                     not null,
+    "Date"        timestamp                                        not null
+);
